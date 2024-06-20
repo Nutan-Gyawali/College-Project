753578f8515c07f2a26af5cff881915ab92fc16e
Nutan-Gyawali <nutangyawali01@gmail.com>
2024-06-20 15:00:15 +0545

    folders created

--- Inventory Management/frontend/src/App.jsx
+++ Inventory Management/frontend/src/App.jsx
@@ -1,13 +1,20 @@
 // src/App.js
 import React from 'react';
-import LoginForm from './components/LoginForm';
+import LoginForm from './components/login/LoginForm';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './page/Home';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
