b9217394f48f9587e55daa20fea6ecad8c3e6fc8
Nutan-Gyawali <nutangyawali01@gmail.com>
2024-08-06 20:19:49 +0545

    css changes

--- sales_report.php
+++ sales_report.php
@@ -1,67 +1,62 @@
 <?php
- include'header.php';
-include'connection.php';
-$t=0;
-if (isset($_POST['submit'])) 
-{
-    $starttime=$_POST['starttime'];
-    $endtime=$_POST['endtime'];
+include 'header.php';
+include 'connection.php';
+$t = 0;
+if (isset($_POST['submit'])) {
+  $starttime = $_POST['starttime'];
+  $endtime = $_POST['endtime'];
 
-$sql = "SELECT * FROM sales where created_at>='$starttime' && created_at<'$endtime'";
-$result = $conn -> query ($sql);
+  $sql = "SELECT * FROM sales where created_at>='$starttime' && created_at<'$endtime'";
+  $result = $conn->query($sql);
 }
 ?>
 <div class="container">
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-  <label for="starttime">Start (date and time):</label>
-  <input type="datetime-local" id="starttime" name="starttime">
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <label for="starttime">Start (date and time):</label>
+    <input type="datetime-local" id="starttime" name="starttime">
 
-  <label for="endtime"> End (date and time):</label>
-  <input type="datetime-local" id="endtime" name="endtime">
-  <input type="submit" name="submit">
-</form>
-<button type="button" onclick="window.print();return false;">Pdf Report</button>
-<div class="container pendingbody">
-  <h5>Sales Report</h5>
-<table class="table">
-  <thead>
-    <tr>
+    <label for="endtime"> End (date and time):</label>
+    <input type="datetime-local" id="endtime" name="endtime">
+    <input type="submit" name="submit">
+  </form>
+  <button type="button" onclick="window.print();return false;">Pdf Report</button>
+  <div class="container pendingbody">
+    <h5>Sales Report</h5>
+    <table class="table table-striped">
+      <thead>
+        <tr>
 
-      <th scope="col">Name</th>
-      <th scope="col">Unit</th>
-      <th scope="col">Total</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-   if(isset($_POST['submit']))
-   {
-          
-  $t=0;
+          <th scope="col">Name</th>
+          <th scope="col">Unit</th>
+          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        if (isset($_POST['submit'])) {
+
+          $t = 0;
           if (mysqli_num_rows($result) > 0) {
             // output data of each row
-            while($row = mysqli_fetch_assoc($result)) {
-                    $t=$t+$row["totalprice"];
+            while ($row = mysqli_fetch_assoc($result)) {
+              $t = $t + $row["totalprice"];
 
-              ?>
-    <tr>
-
-      <td><?php echo $row["name"] ?></td>
-      <td><?php echo $row["sellunit"] ?></td>
-      <td><?php echo $row["totalprice"] ?></td>
-    </tr>
-    <?php 
-
-    }
-
-        } 
-        else 
-            echo "0 results";
-
-    }
         ?>
-  </tbody>
-</table>
-<?php echo "Total= " . $t ." Taka";?>
+              <tr>
 
-</div>
+                <td><?php echo $row["name"] ?></td>
+                <td><?php echo $row["sellunit"] ?></td>
+                <td><?php echo $row["totalprice"] ?></td>
+              </tr>
+        <?php
+
+            }
+          } else
+            echo "0 results";
+        }
+        ?>
+      </tbody>
+    </table>
+    <?php echo "Total= Rs. " . $t; ?>
+
+  </div>
